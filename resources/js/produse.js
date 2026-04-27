0568208796a849659ea7db46795c23ae88717bfe
VladCiobotenco <vlad.ciobotenco@s.unibuc.ro>
2026-04-27 16:05:51 +0300

    Added products filter

---
+++ resources/js/produse.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+    document.getElementById("filtrare").onclick=function(){
+        let inpNume=document.getElementById("inp-nume").value.trim().toLowerCase()
+        console.log(produse)
+        let produse=document.getElementsByClassName("produs")
+        for(let produs of produse){
+            produs.style.display="none"
+            let nume=produs.getElementsByClassName("val-nume")[0].innerHTML.trim().toLowerCase()
+            // e un vector cu un element, trebuie selectat primul element
+            let cond1=nume.includes(inpNume)
+            if(cond1){
+                produs.style.display="block"
+            }
+        }
+
+    }
+}
